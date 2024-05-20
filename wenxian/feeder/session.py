2208f46f13f2b1e9ea7fae664da24c0ffc17ade2
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-05-20 01:33:41 -0400

    apply max_retries to LimiterAdapter

--- wenxian/feeder/session.py
+++ wenxian/feeder/session.py
@@ -8,19 +8,6 @@ from requests.adapters import HTTPAdapter, Retry
 from requests_ratelimiter import LimiterAdapter
 
 SESSION = Session()
-# https://www.ncbi.nlm.nih.gov/books/NBK25497/
-adapter_ncbi = LimiterAdapter(per_second=3)
-SESSION.mount("https://www.ncbi.nlm.nih.gov/pmc/utils/", adapter_ncbi)
-SESSION.mount("https://eutils.ncbi.nlm.nih.gov/", adapter_ncbi)
-# Crossref rate limit is not documented but discussed in
-# https://github.com/CrossRef/rest-api-doc/issues/196
-adapter_crossref = LimiterAdapter(per_second=50)
-SESSION.mount("https://api.crossref.org/", adapter_crossref)
-# Arxiv: https://info.arxiv.org/help/api/tou.html
-adapter_arxiv = LimiterAdapter(
-    limiter=Limiter(RequestRate(1, Duration.SECOND * 3)), burst=1
-)
-SESSION.mount("https://export.arxiv.org/api", adapter_arxiv)
 
 # retry logic
 retries = Retry(
@@ -34,6 +21,21 @@ retries = Retry(
         504,  # Gateway Timeout
     ],
 )
+
+# https://www.ncbi.nlm.nih.gov/books/NBK25497/
+adapter_ncbi = LimiterAdapter(per_second=3, max_retries=retries)
+SESSION.mount("https://www.ncbi.nlm.nih.gov/pmc/utils/", adapter_ncbi)
+SESSION.mount("https://eutils.ncbi.nlm.nih.gov/", adapter_ncbi)
+# Crossref rate limit is not documented but discussed in
+# https://github.com/CrossRef/rest-api-doc/issues/196
+adapter_crossref = LimiterAdapter(per_second=50, max_retries=retries)
+SESSION.mount("https://api.crossref.org/", adapter_crossref)
+# Arxiv: https://info.arxiv.org/help/api/tou.html
+adapter_arxiv = LimiterAdapter(
+    limiter=Limiter(RequestRate(1, Duration.SECOND * 3)), burst=1, max_retries=retries
+)
+SESSION.mount("https://export.arxiv.org/api", adapter_arxiv)
+
 SESSION.mount("https://", HTTPAdapter(max_retries=retries))
 
 __all__ = ["SESSION"]
